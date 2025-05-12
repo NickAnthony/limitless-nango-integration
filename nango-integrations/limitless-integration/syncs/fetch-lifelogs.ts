296e9e30915882cec7f8b3fbc9684e755642f426
Nick Anthony <nickanthony45@gmail.com>
2025-05-11 21:49:31 -0400

    feat: add nango limitless integration scaffolding

---
+++ nango-integrations/limitless-integration/syncs/fetch-lifelogs.ts
@@ -0,0 +1,8 @@
+import type { NangoSync, LifelogList } from '../../models';
+
+export default async function fetchData(nango: NangoSync): Promise<void> {
+    // Integration code goes here
+    // to save data use await nango.batchSave
+    // to delete data use await nango.batchDelete
+}
+
